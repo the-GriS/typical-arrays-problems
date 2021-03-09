54856af253da4fa0f1f8dbe3265ac8755767305d
the-GriS <grishhenjas@gmail.com>
2021-03-09 14:52:49 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min.apply(Math, array);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max.apply(Math, array);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let summary = 0;
+        for (let i = 0; i < array.length; i++) {
+            summary += array[i];
+        }
+        let answer = summary / array.length;
+        return answer;
+    }
+};
